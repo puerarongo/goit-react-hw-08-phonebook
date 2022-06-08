886bec75198fd4d5ad1287cb25981a845f302b87
puerarongo <akhardin96@gmail.com>
2022-06-08 13:10:13 +0300

    fix

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './Loader.module.css';
-import { InfinitySpin } from 'react-loader-spinner';
+import { BallTriangle } from 'react-loader-spinner';
 
 const Loader = () => {
   return (
     <div className={styles.container}>
-      <InfinitySpin color="rgb(0, 0, 0)" height={200} width={200} />
+      <BallTriangle color="rgb(0, 0, 0)" height={200} width={200} />
     </div>
   );
 };
